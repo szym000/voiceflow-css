b31ec79d1790530a4245fb42a19a7ce2a30aed5b
szym000 <165960056+szym000@users.noreply.github.com>
2024-04-22 21:49:50 +0200

    removes style

--- extensions-vg.js
+++ extensions-vg.js
@@ -10,87 +10,17 @@ export const documentDetails = {
         // Set the inner HTML of the form, simplifying it to only include input fields and a submit button
         formContainer.innerHTML = `
         <style>
-        .extensionsForm {
-             background-color: rgb(244, 244, 245) !important;
-             padding: 16px !important;
-             border-radius: 8px !important;
-        }
-        label {
-          font-size: 0.9em !important;
-          color: #888 !important;
-        }
-        .textareaField {
-          font-family: inherit !important;
-          display: block !important;
-          width: 284px !important;
-          height: 100px !important;
-          border-radius: 8px !important;
-          padding: 8px !important;
-          font-size: 0.9em !important;
-          resize: vertical !important;
-          border: 1px solid #ccc !important;
-          margin-bottom: 10px !important;
-        }
-        .textareaField:focus, textarea:focus-visible {
-          outline: 1px solid #71c9ce !important;
-        }
-        .textareaField::placeholder {
-        font-family: inherit; color: #ccc !important;
-        }
-        .textareaField:focus::placeholder {
-          color: transparent !important;
-        }
-        .submit-doc {
-          width: 100% !important;
-          background-color: grey !important;
-          color: white !important;
-          border: none !important;
-          padding: 10px !important;
-          font-weight: 600 !important;
-          border-radius: 8px !important;
-          font-size: 15px !important;
-          cursor: not-allowed !important;
-          opacity: 0.5 !important;
-        }
-        .cancel-doc, .back-doc {
-          width: 100% !important;
-          background-color: #64AFB4 !important;
-          color: #fff !important;
-          font-weight: 600 !important;
-          padding: 7px !important;
-          border-radius: 8px !important;
-          font-size: 15px !important;
-          transition: background-color .4s !important;
-          border: none !important;
-        }
-
-        .active-doc {
-          background-color: #64AFB4 !important;
-          cursor: pointer !important;
-          opacity: 1 !important;
-          transition: background-color .4s !important;
-        }
-        .active-doc:hover, .cancel-doc:hover, .back-doc:hover {
-          background-color: #71C9CE !important;
-        }
-        .button-wrapper {
-          margin-top: 10px !important;
-            gap: 10px !important;
-            display: flex !important;
-            justify-content: space-around !important;
-            align-items: normal !important;
-            width: 100% !important;
-        }
+        
       </style>
 
       <label for="documentDetails">Ihre Informationen/Angaben</label>
       <textarea class="textareaField" id="documentDetails" name="documentDetails" required placeholder="z.B. eine Kopie der Rechnung mit der Nummer 12345"></textarea>
       
-      <input type="submit" class="submit-doc" value="Weiter">
+      <input type="submit" class="submitButton" value="Weiter">
       
       <div class="button-wrapper">
-      <input type="button" class="back-doc" value="❮ Zurück">
-      <input type="button" class="cancel-doc" value="✕ Abbrechen">
+      <input type="button" class="backButton" value="❮ Zurück">
+      <input type="button" class="cancelButton" value="✕ Abbrechen">
       </div>
 `;
 
@@ -98,14 +28,14 @@ export const documentDetails = {
  const checkInput = () => {
     const documentDetails = formContainer.querySelector('#documentDetails').value;
 
-    const submitButton = formContainer.querySelector('.submit-doc');
+    const submitButton = formContainer.querySelector('.submitButton');
     if (documentDetails.trim() !== '') {
       submitButton.disabled = false;
-      submitButton.classList.add('active-doc');
+      submitButton.classList.add('activeButton');
       submitButton.style.cursor = 'pointer';
     } else {
       submitButton.disabled = true;
-      submitButton.classList.remove('active-doc');
+      submitButton.classList.remove('activeButton');
       submitButton.style.cursor = 'not-allowed';
     }
   };
@@ -121,12 +51,12 @@ export const documentDetails = {
       documentDetails.classList.add('invalid');
       return;
     }
-    const buttons = formContainer.querySelectorAll('.submit-doc, .cancel-doc, .back-doc, .active-doc');
+    const buttons = formContainer.querySelectorAll('.submitButton, .cancelButton, .backButton, .activeButton');
             buttons.forEach(button => {
                 button.disabled = true;
-                button.classList.remove('active-doc');
+                button.classList.remove('activeButton');
                 button.style.opacity = '0.5';
-                button.style.cursor = 'default';
+                button.style.cursor = 'not-allowed';
             });
 
    window.VG_ADMIN.interact({
@@ -138,7 +68,7 @@ export const documentDetails = {
   });
 
   // Handle cancel button click
-  formContainer.querySelector('.cancel-doc').addEventListener('click', function () {
+  formContainer.querySelector('.cancelButton').addEventListener('click', function () {
    window.VG_ADMIN.interact({
       type: 'cancel',
       payload: {}
@@ -146,7 +76,7 @@ export const documentDetails = {
   });
 
   // Handle cancel button click
-  formContainer.querySelector('.back-doc').addEventListener('click', function () {
+  formContainer.querySelector('.backButton').addEventListener('click', function () {
    window.VG_ADMIN.interact({
       type: 'back',
       payload: {}
@@ -156,3 +86,250 @@ export const documentDetails = {
         element.appendChild(formContainer); // Append the form to the specified DOM element
     },
 };
+
+
+
+
+
+
+export const patientDataForm = {
+    name: 'ext_patientDataForm',
+    render: ({ trace, element }) => {
+        // Function to render the form
+        console.log(`trace from extension: `, trace)
+        const formContainer = document.createElement('form'); // Create a form element dynamically
+        formContainer.classList.add('extensionsForm');
+  
+      formContainer.innerHTML = `
+        <style>
+          label {
+            font-size: 0.9em;
+            color: #888;
+          }
+          input[type="text"], input[type="email"], input[type="tel"], input[type="date"] {
+            font-family: inherit;
+            width: 284px;
+            border: 1px solid #ccc;
+            background: #fff;
+            margin: 5px 0 10px 0;
+            outline: none;
+            height: 20px;
+            padding: 8px;
+            border-radius: 6px;
+            display: block;
+          }
+          input[type="text"]:focus, input[type="email"]:focus, input[type="tel"]:focus, input[type="date"]:focus {
+            border: 1px solid #71c9ce;
+          }
+          input::placeholder {
+            font-family: inherit; color: #ccc;
+          }
+          input:focus::placeholder {
+            color: transparent;
+          }
+          .invalid {
+            border-color: red;
+          }
+          .submit {
+            width: 100%;
+            background-color: grey;
+            color: white;
+            border: none;
+            padding: 10px;
+            border-radius: 8px;
+            font-size: 15px;
+            font-weight: 600;
+            cursor: not-allowed;
+            opacity: 0.5;
+          }
+          .cancel, .back {
+            width: 100%;
+            background-color: #64AFB4;
+            color: #fff;
+            padding: 7px;
+            border-radius: 8px;
+            font-weight: 600;
+            cursor: pointer;
+            font-size: 15px;
+            transition: background-color .4s;
+            border: none;
+          }
+          
+          .active {
+            background-color: #64AFB4;
+            cursor: pointer;
+            opacity: 1;
+            transition: background-color .4s;
+          }
+          .active:hover, .cancel:hover, .back:hover {
+            background-color: #71C9CE;
+          }
+          .button-wrapper {
+              margin-top: 10px;
+              gap: 10px;
+              display: flex;
+              justify-content: space-around;
+              align-items: normal;
+              width: 100%;
+          }
+  
+          /* Styling for the checkbox container */
+  .checkbox-container {
+    gap: 3px;
+    display: flex;
+    align-items: center; /* Aligns the checkbox with the text vertically */
+    margin-bottom: 20px; /* Bottom margin */
+    margin-top: 20px;
+  }
+  
+  /* Styling for the checkbox input */
+  .checkbox-container input[type="checkbox"] {
+    width: 20px; /* Larger checkbox */
+    height: 20px; /* Larger checkbox */
+    cursor: pointer; /* Pointer cursor on hover */
+    -webkit-appearance: none; /* Removes default styling */
+    -moz-appearance: none; /* Removes default styling */
+    appearance: none; /* Removes default styling */
+    border: 1px solid #ddd; /* Custom border */
+    background-color: white; /* Background color */
+    position: relative; /* Needed for absolute positioning of the pseudo-element */
+    transition: background-color 0.3s, border-color 0.3s; /* Smooth transition for colors */
+  }
+  
+  /* Styling for the checkbox when checked */
+  .checkbox-container input[type="checkbox"]:checked {
+    background-color: #64AFB4; /* Custom background color */
+    border-color: #64AFB4; /* Border color matches background */
+  }
+  
+  /* Styling for the checkmark */
+  .checkbox-container input[type="checkbox"]:checked::after {
+    content: "\\2713"; /* Unicode character for checkmark */
+    color: white; /* White checkmark */
+    position: absolute;
+    left: 50%; /* Center the checkmark horizontally */
+    top: 50%; /* Center the checkmark vertically */
+    transform: translate(-50%, -50%); /* Adjust positioning to true center */
+    font-size: 16px; /* Size of the checkmark */
+    font-weight: bold; /* Makes the checkmark more visible */
+  }
+  
+  /* Styling for the data protection link */
+  .data-link {
+    color: #0000EE; /* Standard link color */
+    text-decoration: none; /* No underline */
+  }
+  
+  .data-link:hover {
+    text-decoration: underline; /* Underline on hover */
+  }
+        </style>
+  
+        <label for="name">Name</label>
+        <input type="text" class="name" name="name" required placeholder="z.B. Max Mustermann">
+  
+        <label for="birthday">Geburtstag</label>
+        <input type="date" class="birthday" name="birthday" required>
+  
+        <label for="email">E-mail</label>
+        <input type="email" class="email" name="email" placeholder="z.B. mustermann@beispiel.de" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" title="Ungültige E-Mail-Adresse">
+  
+        <label for="phone">Telefon</label>
+        <input type="tel" class="phone" name="phone" placeholder="z.B. +491234567890" pattern="\+?\d{7,}" title="Ungültige Telefonnummer, bitte geben Sie mindestens 10 Ziffern ein">
+  
+        <label for="dataProtection" class="checkbox-container">
+        <input type="checkbox" class="accept-terms" id="dataProtection" name="dataProtection" required>
+         Ich akzeptiere die  <a href="#" class="data-link">Datenschutzbestimmungen</a>
+        </label>
+        
+        <input type="submit" class="submit" value="Weiter">
+        <div class="button-wrapper">
+        
+        <input type="button" class="back" value="❮ Zurück">
+        <input type="button" class="cancel" value="✕ Abbrechen">
+        
+        </div>
+      `;
+  
+      const checkInputs = () => {
+        const name = formContainer.querySelector('.name').value;
+        const birthday = formContainer.querySelector('.birthday').value;
+        const email = formContainer.querySelector('.email').value;
+        const phone = formContainer.querySelector('.phone').value;
+      
+        // Additional: Check if the checkbox is checked
+        const isTermsAccepted = formContainer.querySelector('.accept-terms').checked;
+      
+        const isEmailOrPhoneValid = email !== '' || phone !== '';
+        const isFormValid = name && birthday && isEmailOrPhoneValid && isTermsAccepted;
+      
+        const submitButton = formContainer.querySelector('.submit');
+        if (isFormValid) {
+          submitButton.disabled = false;
+          submitButton.classList.add('active');
+          submitButton.style.cursor = 'pointer';
+        } else {
+          submitButton.disabled = true;
+          submitButton.classList.remove('active');
+          submitButton.style.cursor = 'not-allowed';
+        }
+      };
+  
+      formContainer.querySelectorAll('input[type="text"], input[type="email"], input[type="tel"], input[type="date"], input[type="checkbox"]').forEach(input => {
+        if (input.type === "checkbox") {
+          input.addEventListener('change', checkInputs);
+        } else {
+          input.addEventListener('input', checkInputs);
+        }
+      });
+  
+      formContainer.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = formContainer.querySelector('.name');
+        const birthday = formContainer.querySelector('.birthday');
+        const email = formContainer.querySelector('.email');
+        const phone = formContainer.querySelector('.phone');
+  
+        if (
+          !name.checkValidity() ||
+          !birthday.checkValidity() ||
+          !email.checkValidity() ||
+          !phone.checkValidity()
+        ) {
+          name.classList.add('invalid');
+          birthday.classList.add('invalid');
+          email.classList.add('invalid');
+          phone.classList.add('invalid');
+          return;
+        }
+  
+        window.VG_ADMIN.interact({
+          type: 'complete',
+          payload: {
+            name: name.value,
+            birthday: birthday.value,
+            email: email.value,
+            phone: phone.value
+          },
+        });
+      });
+  
+      // Handle cancel button click
+      formContainer.querySelector('.cancel').addEventListener('click', function () {
+        window.VG_ADMIN.interact({
+          type: 'cancel',
+          payload: {}
+        });
+      });
+      
+      // Handle cancel button click
+      formContainer.querySelector('.back').addEventListener('click', function () {
+        window.VG_ADMIN.interact({
+          type: 'back',
+          payload: {}
+        });
+      });
+  
+      element.appendChild(formContainer);
+    },
+  };
